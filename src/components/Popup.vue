409be847f256e77206d21dd09c63602adc9f4233
Neowu2021 <wuhongyi2011@gmail.com>
2022-02-16 14:56:59 +0800

    巫弘毅 面試

---
+++ src/components/Popup.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="popup">
+    <div class="mask"></div>
+    <div class="pop-container">
+      <div class="content">
+        <div class="form-area">
+          <slot></slot>
+        </div>
+        <div class="close-btn" @click="closePop">
+          X
+        </div>
+        {{count}}
+        <div class="add-btn" @click="increase">add</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Popup",
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  mounted() {
+    console.log(this.count)
+  },
+  methods: {
+    closePop() {
+      this.$emit('closePop')
+    },
+    increase () {
+      this.$store.dispatch('increment')
+    }
+  }
+};
+
+</script>
+<style lang="scss" scoped>
+#popup {
+  .pop-container {
+    width: 300px;
+    height: 300px;
+    background-color: #FFF;
+    position: absolute;
+    z-index: 1000;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    .content {
+      position: relative;
+      .add-btn {
+        width: 50px;
+        margin: 0 auto;
+        background-color: yellow;
+      }
+      .close-btn {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 30px;
+        height: 30px;
+        background-color: red;
+        border-radius: 15px;
+      }
+    }
+  }
+
+  .mask {
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 999;
+  }
+}
+
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>隨機1~5 請看console</h1>
+
   </div>
 </template>
+<script>
+  export default {
+    name: 'About',
+    data() {
+      return {
+        testArray: [1,2,3,4,5]
+      }
+    },
+    mounted() {
+      let that = this
+      setInterval(() => {
+        let randonIndex = Math.floor(Math.random() * that.testArray.length);
+        console.log(that.testArray[randonIndex])
+        that.testArray.splice(randonIndex, 1)
+        if (that.testArray.length === 0) {
+          that.testArray = [1,2,3,4,5]
+        }
+      }, 1000)
+    }
+  }
+</script>
